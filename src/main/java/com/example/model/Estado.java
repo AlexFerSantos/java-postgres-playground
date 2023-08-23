620ccec471dd6c2fa835204a59ca4d4f4ffc42ec
Alex Ferreira <allexsantos@hotmail.com>
2023-08-23 05:51:46 +0000

    refatorando

---
+++ src/main/java/com/example/model/Estado.java
@@ -0,0 +1,48 @@
+package com.example.model;
+
+public class Estado {
+    private Long id;
+    private String nome;
+    private String uf;
+    private RegiaoGeografica regiao;
+    private int areaKm;
+    private int populacao;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getUf() {
+        return uf;
+    }
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    public RegiaoGeografica getRegiao() {
+        return regiao;
+    }
+    public void setRegiao(RegiaoGeografica regiao) {
+        this.regiao = regiao;
+    }
+    public int getAreaKm() {
+        return areaKm;
+    }
+    public void setAreaKm(int areaKm) {
+        this.areaKm = areaKm;
+    }
+    public int getPopulacao() {
+        return populacao;
+    }
+    public void setPopulacao(int populacao) {
+        this.populacao = populacao;
+    }
+    
+}
